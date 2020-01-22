2316d7cdf51d2491d3d1f68f6cf96c2c2dc79535
Igor Khan <i_m@khanigo.pw>
2020-01-22 14:39:32 +1000

    no message

--- build/assets/js/blocks.js
+++ build/assets/js/blocks.js
@@ -4,12 +4,15 @@
 
 
 const ham = document.querySelector('.js-ham'),
+      nav = document.querySelector('.c-header__nav'),
       hamCallback = function(e) {
           e.preventDefault();
           this.classList.toggle("is-active");
+          nav.classList.toggle("is-active");
       };
 
 ham.addEventListener('click', hamCallback);
+
 
 
 function getPopupId(classes) {
@@ -42,5 +45,4 @@ jQuery(function($) {
     }, 50 );
     
 });
-
 
--- src/pug/ui/ui-hamburger/_ui-hamburger.js
+++ src/pug/ui/ui-hamburger/_ui-hamburger.js
@@ -1,7 +1,9 @@
 const ham = document.querySelector('.js-ham'),
+      nav = document.querySelector('.c-header__nav'),
       hamCallback = function(e) {
           e.preventDefault();
           this.classList.toggle("is-active");
+          nav.classList.toggle("is-active");
       };
 
 ham.addEventListener('click', hamCallback);
